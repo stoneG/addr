c026f7eb8f2ac507f517cf4e4529495d2418ca50
stoneG <me@sitong>
2013-02-25 21:24:08 -0800

    building the db model

---
+++ books/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Addresses(models.Model):
+    name = models.CharField(max_length=100)
+    line_one = models.CharField(max_length=100)
+    line_two = models.CharField(max_length=50)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=2)
+    zip_code = models.CharField(max_length=5)
+    user = models.ForeignKey(User)
