f2f1faa9f8265b3c7bb680ae2cbe76ad35986fca
stoneG <me@sitong>
2013-02-27 01:18:23 -0800

    Making google maps position:fixed using the !important tag

--- addr/settings.py
+++ addr/settings.py
@@ -62,7 +62,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/sitong/Dropbox/Projects/addr/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -70,6 +70,8 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+        os.getcwd()[:-4] + "templates/static", # addr is 4 characters long
+        os.getcwd() + "/templates/static",
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
--- books/views.py
+++ books/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def main(request):
-    return render_to_response('main.html')
+    return render_to_response('main.html',
+                              context_instance=RequestContext(request))
