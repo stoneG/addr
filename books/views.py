73242c05952762aa81e9f868287bdc9542ff9d3e
stoneG <me@sitong>
2013-02-27 17:46:21 -0800

    testing the style layout

--- books/views.py
+++ books/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def main(request):
-    return render_to_response('main.html',
+    return render_to_response('main.html', {'addresses':[]},
                               context_instance=RequestContext(request))
